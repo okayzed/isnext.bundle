e606ab3bdbb55d4357c404ac71465ef4d97be7a3
okay <okay@chalk>
2019-10-08 18:14:36 -0700

    add play_progression

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -4510,32 +4510,65 @@ window.IS = IS;
 
 },{"./player.js":28,"isnext":18}],28:[function(require,module,exports){
 var normal = require("isnext").normal;
-var T = require("tone");
+var Tone = require("tone");
 var teoria = require("teoria");
 
 
-var polySynth = new T.PolySynth(4, T.Synth).toMaster();
+var polySynth = new Tone.PolySynth(4, Tone.Synth).toMaster();
 
 
+function get_chord(chord) {
+  var teoria_chord;
+  try {
+  chord = normal.get_normal_chord(chord.name || chord);
+  teoria_chord = teoria.chord(chord);
+  } catch(e) {
+    console.log("CANT DO CHORD", chord, e);
+    return;
+  }
+
+  var chord_notes = teoria_chord.notes().map(function(n) { return n.toString() });
+  return chord_notes;
+}
+
+var chordPart;
 module.exports = {
   play_chord: function(chord, duration) {
-    try {
-    chord = normal.get_normal_chord(chord.name || chord);
-    var teoria_chord = teoria.chord(chord);
-    } catch(e) {
-      console.log("CANT DO CHORD", chord, e);
-      return;
-    }
-    duration = duration || 1000;
+    duration = duration || "4n"
 
     // octaves cost 12. so... the offsets will be...
-    var chord_notes = teoria_chord.notes().map(function(n) { return n.toString() });
-    polySynth.triggerAttackRelease(chord_notes, "2n");
+    var chord_notes = get_chord(chord);
+    console.log("PLAYING", chord, chord_notes);
+    polySynth.triggerAttackRelease(chord_notes, duration);
+  },
+  play_progression: function(chords) {
+      chords = chords.map(function(c) { return get_chord(c); });
+      chords = chords.map(function(c, i) { return [i, c] });
+      console.log("PLAYING PROGRESSION", chords);
+      if (chordPart) {
+        chordPart.stop(0);
+      }
+
+      chordPart = new Tone.Part(function(time, chord){
+        console.log("PLAYING PART", chord);
+        if (chord) {
+          polySynth.triggerAttackRelease(chord, "2n");
+        }
+      }, chords);
+
+      chordPart.loop = false;
+      chordPart.start(0);
+      Tone.Transport.bpm.value = 100;
+      Tone.Transport.stop();
+      Tone.Transport.start("+0.1");
+
+
   }
 };
 
 
 window.play_chord = module.exports.play_chord;
+window.play_progression = module.exports.play_progression;
 
 },{"isnext":18,"teoria":8,"tone":16}],29:[function(require,module,exports){
 // shim for using process in browser
--- src/js/player.js
+++ src/js/player.js
@@ -1,27 +1,60 @@
 var normal = require("isnext").normal;
-var T = require("tone");
+var Tone = require("tone");
 var teoria = require("teoria");
 
 
-var polySynth = new T.PolySynth(4, T.Synth).toMaster();
+var polySynth = new Tone.PolySynth(4, Tone.Synth).toMaster();
 
 
+function get_chord(chord) {
+  var teoria_chord;
+  try {
+  chord = normal.get_normal_chord(chord.name || chord);
+  teoria_chord = teoria.chord(chord);
+  } catch(e) {
+    console.log("CANT DO CHORD", chord, e);
+    return;
+  }
+
+  var chord_notes = teoria_chord.notes().map(function(n) { return n.toString() });
+  return chord_notes;
+}
+
+var chordPart;
 module.exports = {
   play_chord: function(chord, duration) {
-    try {
-    chord = normal.get_normal_chord(chord.name || chord);
-    var teoria_chord = teoria.chord(chord);
-    } catch(e) {
-      console.log("CANT DO CHORD", chord, e);
-      return;
-    }
-    duration = duration || 1000;
+    duration = duration || "4n"
 
     // octaves cost 12. so... the offsets will be...
-    var chord_notes = teoria_chord.notes().map(function(n) { return n.toString() });
-    polySynth.triggerAttackRelease(chord_notes, "2n");
+    var chord_notes = get_chord(chord);
+    console.log("PLAYING", chord, chord_notes);
+    polySynth.triggerAttackRelease(chord_notes, duration);
+  },
+  play_progression: function(chords) {
+      chords = chords.map(function(c) { return get_chord(c); });
+      chords = chords.map(function(c, i) { return [i, c] });
+      console.log("PLAYING PROGRESSION", chords);
+      if (chordPart) {
+        chordPart.stop(0);
+      }
+
+      chordPart = new Tone.Part(function(time, chord){
+        console.log("PLAYING PART", chord);
+        if (chord) {
+          polySynth.triggerAttackRelease(chord, "2n");
+        }
+      }, chords);
+
+      chordPart.loop = false;
+      chordPart.start(0);
+      Tone.Transport.bpm.value = 100;
+      Tone.Transport.stop();
+      Tone.Transport.start("+0.1");
+
+
   }
 };
 
 
 window.play_chord = module.exports.play_chord;
+window.play_progression = module.exports.play_progression;
